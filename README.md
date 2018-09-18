88baf69143824d253681e823a6033c88536283ea
Luiz Filipe Machado Barni <machado@odahcam.com>
2018-09-17 21:17:45 -0300

    added simple Cypress tests

--- README.md
+++ README.md
@@ -8,7 +8,7 @@
 
 > If it doesn't, we gonna make it.
 
-## What is ZXing?
+## What is ZXing
 
 > [ZXing][1] ("zebra crossing") is an open-source, multi-format 1D/2D barcode image processing library implemented in Java, with ports to other languages.
 
---
+++ test/integration/sample_test.spec.ts
@@ -0,0 +1,10 @@
+import 'cypress';
+
+// import { BrowserBarcodeReader } from '../../src/BrowserBarcodeReader';
+
+describe('My First Test', () => {
+    it('Does not do much!', () => {
+        // const test = new BrowserBarcodeReader();
+        expect(true).to.equal(true);
+    });
+});
